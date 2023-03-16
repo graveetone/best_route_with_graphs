e75cf5e413a7cb7aa3e8a850dc05b359d891d53e
graveetone <zastavskijs@gmail.com>
2023-03-16 13:24:43 +0200

    work in progress

---
+++ pytimer.py
@@ -0,0 +1,14 @@
+import time
+
+class Timer:
+    def __init__(self):
+        self._start_time = time.time()
+        self.stopped = False
+    
+    def _stop(self):
+        self.timedelta = round((time.time() - self._start_time), 3)
+        self.stopped = True
+
+    def __repr__(self):
+        self._stop()
+        return f"{self.timedelta} секунд"       
