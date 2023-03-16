c10c79dbe56b62cd4ab4b14824e446f363ac2705
graveetone <zastavskijs@gmail.com>
2023-03-16 13:23:39 +0200

    deleted outdated files

--- app/services/logger_service.py
+++
@@ -1,6 +0,0 @@
-from services.config_service import ConfigService
-class Loger:
-    @staticmethod
-    def log(message, *args):
-        if ConfigService.LOGGER_ENABLED:
-            print(f"{message}: {args}")
